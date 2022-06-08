7a773819328bd199c23ed2eb439fd20a60d2cacd
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-06-08 14:07:37 -0400

    commit tree e7aefa022cf07fc910e4508c27d99785b1d63742

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+const body = document.querySelector('body')
+let message = 'Unauthorized Access'
+
+function submitData(name, email){
+    
+    const configurationObject = {
+    method: 'POST', 
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+        'name': name,
+        'email': email
+    })
+    }
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(res => res.json())
+    .then(body => addNewUser(body.id))
+    .catch(error => errorMessage(error.message))
+
+    function addNewUser(newID){
+        document.querySelector('body').innerHTML = newID
+    }
+
+    function errorMessage(message){
+        document.body.innerHTML = message 
+    }
+}
+
+submitData('name', 'email')
